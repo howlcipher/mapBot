f69bda1cf734a2845a1afeb96c9876b1938bb710
William Elias <wylelias.123@gmail.com>
2020-05-17 14:19:02 -0400

    Connect the application to a database and query it for data to be used in the bot

---
+++ users.py
@@ -0,0 +1,19 @@
+import numpy as np
+import psycopg2
+
+# database
+conn = psycopg2.connect(
+    database="mapBot",
+    user="postgres",
+    password="54321",
+    host="localhost",
+    port=5432
+)
+
+# testing sql query
+print("connected to postgres SQL USER_ACCOUNT DB")
+cur = conn.cursor()
+data = cur.execute("SELECT username, bot_admin, server_role FROM user_account ORDER BY user_id")
+maps = cur.fetchall()
+
+userdb = np.array(maps)
